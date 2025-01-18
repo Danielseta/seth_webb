53561b4789650f9dc12c1b4ecdf2523bc9f18de7
Daniel Seta <99261658+Danielseta@users.noreply.github.com>
2025-01-18 12:53:04 -0800

    Update icons.tsx

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,6 +1,6 @@
 import { GlobeIcon, MailIcon } from "lucide-react";
 
-export type IconProps = React.HTMLAttributes<SVGElement>;
+export type IconProps = Reac;
 
 export const Icons = {
   globe: (props: IconProps) => <GlobeIcon {...props} />,
