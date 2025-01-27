25dd6d8f320854d520467564fd4fe399ac08a6b7
Daniel Seta <99261658+Danielseta@users.noreply.github.com>
2025-01-27 12:27:59 -0800

    Update separator.tsx

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -10,9 +10,7 @@ const Separator = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
 >(
   (
-    { className, orientation = "horizontal", decorative = true, ...props },
-    ref
-      decorative={decorative}
+    { className, orientation = "horizontal", decorative = true, ...prop
       orientation={orientation}
       className={cn(
         "shrink-0 bg-border",
