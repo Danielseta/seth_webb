f8d04b2b532d4b3cbe96fb8360e62bf569905d41
Daniel Seta <99261658+Danielseta@users.noreply.github.com>
2025-01-31 12:32:36 -0800

    Update hackathon-card.tsx

--- src/components/hackathon-card.tsx
+++ src/components/hackathon-card.tsx
@@ -7,9 +7,7 @@ interface Props {
     <li className="relative ml-10 py-4">
       <div className="absolute -left-16 top-2 flex items-center justify-center bg-white rounded-full">
         <Avatar className="border size-12 m-auto">
-          <AvatarImage src={image} alt={title} 
-     
-      
+          <Avatar
       
       </div>
       {links && links.length > 0 && (
