810fe6679305c36748d015abac0ed16f9b6ad329
Daniel Seta <99261658+Danielseta@users.noreply.github.com>
2025-02-01 09:46:04 -0800

    Update hackathon-card.tsx

--- src/components/hackathon-card.tsx
+++ src/components/hackathon-card.tsx
@@ -8,7 +8,7 @@ interface Props {
       <div className="absolute -left-16 top-2 fl
       </div>
       {links && links.length > 0 && (
-        <div className="mt-2 flex flex-row flex-wrap items-start gap-2">
+        <div classNarap items-start gap-2">
           {links?.map((link, idx) => (
             <Link href={link.href} key={idx}>
               <Badge key={idx} title={link.title} className="flex gap-2">
