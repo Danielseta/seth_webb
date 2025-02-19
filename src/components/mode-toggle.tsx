a940a1ece013883a99ad75edcf70c54b05bdcccd
Daniel Seta <99261658+Danielseta@users.noreply.github.com>
2025-02-19 13:10:22 -0800

    Update mode-toggle.tsx

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { MoonIc
  
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      onClick={() =>
     >
       <SunIcon className="h-[1.2rem] w-[1.2rem] rk:text-neutral-200" />
       <MoonIcon className="hidden h-[1.2rem] w-[1.2rem] text-neutral-800 dark:block dark:text-n
