31bd04502617558c87a41388a7a2105880e444a3
Daniel Seta <99261658+Danielseta@users.noreply.github.com>
2025-02-21 12:03:29 -0800

    Update hackathon-card.tsx

--- src/components/hackathon-card.tsx
+++ src/components/hackathon-card.tsx
@@ -5,9 +5,7 @@ import Link from "next/link";
 interface Props {
   title: str
     <li className="relative ml-10 py-4">
-      <div className="absolute -left-16 top-2 fl
-      </div>
-    
+     
       )}
     </li>
   );
