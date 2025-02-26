24df8fd43d287b77f618d98142da48ba2a8fb411
Daniel Seta <99261658+Danielseta@users.noreply.github.com>
2025-02-26 13:53:13 -0800

    Update hackathon-card.tsx

--- src/components/hackathon-card.tsx
+++ src/components/hackathon-card.tsx
@@ -1,6 +1,3 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
-import Link from "next/link";
-
-interface Props {
-  title: s
+import Link 
